4ce9d7888d2b79deb611586154dbf53736083e0d
rawdaGastan <rody.rawda.rr@gmail.com>
2023-10-31 13:43:08 +0200

    use edit url

--- pkg/httpd/webclient.go
+++ pkg/httpd/webclient.go
@@ -727,8 +727,7 @@ func (s *httpdServer) renderEditFilePage(w http.ResponseWriter, r *http.Request,
 				return
 			}
 			path := url.QueryEscape(fileName)
-			tokenString := jwtauth.TokenFromCookie(r)
-			documentURL += fmt.Sprintf("/api/v2/user/files?path=%s&jwt=%s", path, tokenString)
+			documentURL += fmt.Sprintf("%v?path=%v&_=%v", webClientFilesPath, path, time.Now().UTC().Unix())
 		}
 		name := connection.User.GetCleanedPath(fileName)
 		info, err := connection.Stat(name, 0)
@@ -1153,15 +1152,13 @@ func (s *httpdServer) handleClientGetDirContents(w http.ResponseWriter, r *http.
 						Paths:    []string{name + info.Name()},
 						Username: claims.Username,
 					}
-					res["share_url"] = strings.Replace(res["url"].(string), webClientFilesPath, userSharesPath, 1) + fmt.Sprintf("&jwt=%s", jwtauth.TokenFromCookie(r))
+					res["share_url"] = strings.Replace(res["url"].(string), webClientFilesPath, userSharesPath, 1) + fmt.Sprintf("&jwt=%s&_=%v", jwtauth.TokenFromCookie(r), time.Now().UTC().Unix())
 					jsonShare, err := json.Marshal(share)
 					if err != nil {
 						sendAPIResponse(w, r, err, "Unable to marshal share", getMappedStatusCode(err))
 						return
 					}
 					res["share_body"] = string(jsonShare)
-					res["edit_shared_url_without_id"] = getFileObjectURL(name, info.Name(),
-						webClientEditFilePath) + "&id="
 				}
 				if len(s.binding.WebClientIntegrations) > 0 {
 					extension := path.Ext(info.Name())
